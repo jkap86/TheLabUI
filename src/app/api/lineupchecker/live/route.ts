6a80925e77603f7a5bc75caa23b569fcceb97109
Jay Kapoor <jkap86@gmail.com>
2025-08-31 10:45:18 -0400

    fix idp allplayers bug

--- src/app/api/lineupchecker/live/route.ts
+++ src/app/api/lineupchecker/live/route.ts
@@ -1,7 +1,9 @@
-import { NextRequest } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 
 export async function GET(req: NextRequest) {
   const { searchParams } = new URL(req.url);
 
   const week = searchParams.get("week");
+
+  return NextResponse.json(week);
 }
